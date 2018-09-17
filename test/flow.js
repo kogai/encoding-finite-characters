48d30b5333e11603442c3e4c11c9e2b65edd4318
Shinichi Kogai <kogai0121@gmail.com>
2018-09-17 16:40:28 +0900

    :police_car: Add type test for flow

---
+++ test/flow.js
@@ -0,0 +1,13 @@
+// @flow
+import { createEncodingTable } from "encoding-finite-characters.macro";
+
+const converter = createEncodingTable({
+  characters: "価格税込み報酬1234567890-,. \n",
+  from: "UNICODE",
+  to: "SJIS"
+});
+
+(converter: { [string]: number[] });
+
+// $ExpectError
+(converter[""]: boolean);
